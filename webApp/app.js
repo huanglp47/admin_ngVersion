8f77416fc0137c8155638952bf0dca54c829b09b
huanglp47 <huanglip0621@163.com>
2016-03-17 18:44:06 +0800

    Signed-off-by: sss

--- webApp/app.js
+++ webApp/app.js
@@ -1,4 +1,6 @@
 //'use strict';
+//数组有重复值时ng-repeat不加track by $index会报错(加orderBy也可以)
+//var dataLi = ['分类列表','分类列表', '书籍列表', '专题列表','检查书籍','搜索排行','多听资源', '版权列表', '书籍分类推荐'];
 var dataLi = ['分类列表', '书籍列表', '专题列表','检查书籍','搜索排行','多听资源', '版权列表', '书籍分类推荐'];
 angular.module('myApp', ['ui.router'])
     .controller('GetNavLi',function($scope){
@@ -53,9 +55,58 @@ angular.module('myApp', ['ui.router'])
             url: "/list",
             templateUrl: "partials/state2.list.html",
             controller: function($scope) {
-                $scope.items = ["A", "Set", "Of", "Things"];
+                var data = [{
+                    'name': 'Billy Joel',
+                    'zan':'75000'
+                },{
+                    'name': 'Paul McCartney',
+                    'zan':'112000'
+                },{
+                    'name': 'Haul McCartney',
+                    'zan':'1100'
+                },{
+                    'name': 'Ailly Joel',
+                    'zan':'500'
+                }];
+
+                initData(data);
+
+                function initData(data){
+                    for(var i= 0,len=data.length;i<len;i++){
+                        if(data[i]&&data[i].zan){
+                            data[i].zan = transformNum(parseInt(data[i].zan));
+                        }
+                    }
+                };
+                function transformNum(n){
+                    var num = null;
+                    if(n>=0 && n<1000){
+                        num = n;
+                    }else if(n>=1000 && n<10000){
+                        num = (n/1000).toFixed(2)+'k';
+                    }else{
+                        num = (n/10000).toFixed(2)+'w';
+                    }
+                    return num
+                };
+                $scope.items = data;
+
+                $scope.delete = function($index){
+                    if(confirm('确定要删除？')){
+                        $scope.items.splice($index, 1);
+                    }
+                };
+
+                $scope.order = 'name';
+
             }
-        })
+        }).state('editSinger', {
+                url: "/editSinger",
+                templateUrl: "partials/editSinger.html",
+                controller: function($scope) {
+
+                }
+            })
     })
     .controller('ModalDemoCtrl', function($scope, $uibModal, $log){
 
